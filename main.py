3ad0521b03e481df0c284c101766e53cb3ea1e91
falconraptor <falconraptor@users.noreply.github.com>
2018-09-27 19:01:53 -0700

    Mod File Changelog + file_dependencies table + get_mod_file_changelog + time logging to check how long each method takes

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from datetime import datetime
+
 from db import startup
-from mods import get_mods, get_versions, get_mod_details, get_mod_files
+from mods import get_mods, get_versions, get_mod_details, get_mod_files, get_mod_file_changelog
 
 if __name__ == '__main__':
+    total = datetime.now()
+    _start = datetime.now()
     c = startup()
+    start = datetime.now()
+    print('db', start - _start)
     get_versions(c)
+    _start = datetime.now()
+    print('versions', _start - start)
     get_mods(c, '1.12.2')
+    start = datetime.now()
+    print('mods', start - _start)
     get_mod_details(c, 'Quark')
-    get_mod_files(c, 'Quark')  # ~20 seconds
+    _start = datetime.now()
+    print('mod_details', _start - start)
+    get_mod_files(c, 'Quark')
+    start = datetime.now()
+    print('mod_files', start - _start)
+    get_mod_file_changelog(c, 2620164)
+    _start = datetime.now()
+    print('file_changelog', _start - start)
+    print('total', _start - total)
