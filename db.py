9f06b2baedbea3c056d253c95cd85e65cd0f513f
falconraptor <falconraptor@users.noreply.github.com>
2018-10-06 11:43:20 -0700

    Started UI

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
+from os import mkdir
+from os.path import exists
 from sqlite3 import connect, Row, PARSE_DECLTYPES, PARSE_COLNAMES
 
 
@@ -23,7 +25,7 @@ def create_tables(c):
         CREATE TABLE IF NOT EXISTS Modpack (Name VARCHAR(255) PRIMARY KEY ASC, Short_Description VARCHAR(255) NOT NULL, Description TEXT NOT NULL DEFAULT '', Downloads INTEGER NOT NULL, ID INTEGER, IMG_URL TEXT NOT NULL, URL TEXT NOT NULL, Last_Updated TIMESTAMP NOT NULL, Created TIMESTAMP, Last_Checked TIMESTAMP, Last_Description TIMESTAMP, Wiki TEXT NOT NULL DEFAULT '', Issue_Tracker TEXT NOT NULL DEFAULT '', Source TEXT NOT NULL DEFAULT '');
         CREATE TABLE IF NOT EXISTS Modpack_Category (Name VARCHAR(255) PRIMARY KEY ASC, IMG_URL TEXT NOT NULL, URL TEXT NOT NULL);
         CREATE TABLE IF NOT EXISTS Modpack_Category_Map (ID INTEGER PRIMARY KEY, Mod VARCHAR(255) NOT NULL, Category VARCHAR(255) NOT NULL);
-        CREATE TABLE IF NOT EXISTS Modpack_File (ID INTEGER PRIMARY KEY DESC, Mod VARCHAR(255) NOT NULL, Name TEXT NOT NULL, Size INTEGER NOT NULL, Uploaded TIMESTAMP NOT NULL, Downloads INTEGER NOT NULL, Type VARCHAR(1) NOT NULL DEFAULT 'R', Uploaded_By VARCHAR(255) NOT NULL DEFAULT '', Changelog TEXT NOT NULL DEFAULT '', Server_ID INTEGER, Server_Size VARCHAR(255) NOT NULL DEFAULT '', Server_Downloads INTEGER);
+        CREATE TABLE IF NOT EXISTS Modpack_File (ID INTEGER PRIMARY KEY DESC, Mod VARCHAR(255) NOT NULL, Name TEXT NOT NULL, Size INTEGER NOT NULL, Uploaded TIMESTAMP NOT NULL, Downloads INTEGER NOT NULL, Type VARCHAR(1) NOT NULL DEFAULT 'R', Uploaded_By VARCHAR(255) NOT NULL DEFAULT '', Changelog TEXT NOT NULL DEFAULT '', Server_ID INTEGER, Server_Size VARCHAR(255) NOT NULL DEFAULT '', Server_Downloads INTEGER, Installed BOOLEAN DEFAULT FALSE, Last_Played TIMESTAMP);
         CREATE TABLE IF NOT EXISTS Modpack_File_Version (ID INTEGER PRIMARY KEY, File_ID INTEGER NOT NULL, Version VARCHAR(255) NOT NULL);
         CREATE TABLE IF NOT EXISTS Modpack_File_Dependency (ID INTEGER PRIMARY KEY, File_ID INTEGER NOT NULL, Dependency VARCHAR(255) NOT NULL);
         CREATE TABLE IF NOT EXISTS Modpack_Member (ID INTEGER PRIMARY KEY, Mod VARCHAR(255) NOT NULL, Member VARCHAR(255) NOT NULL, Type VARCHAR(100) NOT NULL DEFAULT 'Owner');
@@ -34,7 +36,9 @@ def create_tables(c):
 
 
 def startup():
-    c = connect('client.db', detect_types=PARSE_DECLTYPES | PARSE_COLNAMES, check_same_thread=False)
+    if not exists('launcher'):
+        mkdir('launcher')
+    c = connect('launcher/client.db', detect_types=PARSE_DECLTYPES | PARSE_COLNAMES, check_same_thread=False)
     c.row_factory = MyRow
     create_tables(c)
     return c
--- main.py
+++ main.py
@@ -1,43 +1,49 @@
-from datetime import datetime
+from os.path import dirname, abspath, join
+
+from remi import App, start
+from remi.gui import *
 
 from db import startup
-from modpacks import get_modpack_versions, get_modpacks, get_modpack_details, get_modpack_files, get_modpack_file_changelog
-from mods import get_mods, get_mod_versions, get_mod_details, get_mod_files, get_mod_file_changelog
+
+
+class MyApp(App):
+    def __init__(self, *args, **kwargs):
+        self.c = startup()
+        super().__init__(*args, static_file_path=join(dirname(abspath(__file__)), 'res'), **kwargs)
+
+    def main(self):
+        return self.modpacks()
+
+    def modpacks(self):
+        base = TabBox()
+        base.get_child('ul').style['margin'] = '0'
+        mine = Widget()
+
+        for installed in self.c.execute("SELECT DISTINCT Modpack.Name AS Mod, Modpack.IMG_URL, Modpack_Member.Member, Modpack_File.Name AS File FROM Modpack INNER JOIN Modpack_File ON Modpack.Name==Modpack_File.Mod INNER JOIN Modpack_Member ON Modpack_Member.Mod=Modpack.Name WHERE Modpack_Member.Type=='Owner' AND Modpack_File.Installed==1 ORDER BY Modpack_File.Last_Played"):
+            mod = HBox(Image(installed.IMG_URL, height=200, width=200))
+            mod.add_child('name', Label(installed.Mod))
+            mod.add_child('version', Label('Version: {}'.format('.'.join(installed.File.split('-')[-1].split('.')[:-1]).strip())))
+            mod.add_child('owner', Label('By: {}'.format(installed.Member)))
+            mine.add_child(installed.Mod, mod)
+
+        browse = Widget()
+        head = HBox()
+        refresh = Button('Refresh')
+        search = Input('text')
+        head.add_child('refresh', refresh)
+        head.add_child('search', search)
+        browse.add_child('head', head)
+
+        create = Widget()
+
+        base.add_tab(mine, 'Installed Modpacks', None)
+        base.add_tab(browse, 'Browse Modpacks', None)
+        base.add_tab(create, 'Create Modpacks', None)
+        return base
+
+    def set_different_root_widget(self, emitter, new_root):
+        self.set_root_widget(new_root)
+
 
 if __name__ == '__main__':
-    total = datetime.now()
-    _start = datetime.now()
-    c = startup()
-    start = datetime.now()
-    print('db', start - _start)
-    get_mod_versions(c)
-    _start = datetime.now()
-    print('mod versions', _start - start)
-    get_mods(c, '1.12.2')
-    start = datetime.now()
-    print('mods', start - _start)
-    get_mod_details(c, 'Quark')
-    _start = datetime.now()
-    print('mod details', _start - start)
-    get_mod_files(c, 'Quark')
-    start = datetime.now()
-    print('mod files', start - _start)
-    get_mod_file_changelog(c, 2620164)
-    _start = datetime.now()
-    print('mod file changelog', _start - start)
-    get_modpack_versions(c)
-    start = datetime.now()
-    print('modpack versions', start - _start)
-    get_modpacks(c, '1.12.2')
-    _start = datetime.now()
-    print('modpacks', _start - start)
-    get_modpack_details(c, 'SevTech: Ages')
-    start = datetime.now()
-    print('modpack details', start - _start)
-    get_modpack_files(c, 'SevTech: Ages')
-    _start = datetime.now()
-    print('modpack files', _start - start)
-    get_modpack_file_changelog(c, 2570732)
-    start = datetime.now()
-    print('modpack file changelog', start - _start)
-    print('total', start - total)
+    start(MyApp)  # , standalone=True, resizable=False
