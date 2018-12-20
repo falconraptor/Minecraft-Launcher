3fcfb348680a68776df3947392fc19f86f4d8d05
falconraptor <falconraptor@users.noreply.github.com>
2018-12-20 01:40:23 -0800

    + Started UI + Local copies of Bootstrap 4 and jQuery to speed up runtimes + Able to freeze using python -OOm PyInstaller main.spec - Removed port.js [not needed]

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from random import randint
+import sys
 from threading import Thread
 
-import webview
+from webview import create_window
 
-from webserver import start_server, route
+from webserver import start_server, route, serve
+
+
+@route('/')
+def installed(request):
+    return serve(getattr(sys, '_MEIPASS', '.') + '/public/html/installed.html')
 
 
 @route()
-def index(request):
-    return 'Test'
+def all(request):
+    return serve(getattr(sys, '_MEIPASS', '.') + '/public/html/all.html')
+
+
+@route()
+def settings(request):
+    return serve(getattr(sys, '_MEIPASS', '.') + '/public/html/settings.html')
+
+
+@route(r'/static/([\w.\-/]+)', no_end_slash=True)
+def static(request, file):
+    return serve(getattr(sys, '_MEIPASS', '.') + '/public/' + file)
 
 
 if __name__ == '__main__':
-    port = randint(6000, 32000)
-    with open('public/js/port.js', 'wt') as _out:
-        _out.write('const PORT = {}'.format(port))
+    port = 5674  # randint(6000, 32000)
     server = start_server(bind='127.0.0.1', port=port, serve=False)
-    Thread(target=server.serve_forever, args=(.1,)).start()
-    webview.create_window('Minecraft Launcher', 'http://localhost:{}'.format(port))
+    Thread(target=server.serve_forever).start()
+    win = create_window('Minecraft Launcher', 'http://127.0.0.1:{}'.format(port), background_color='#000000')
     server.shutdown()
